7b45a17bd0473342b9b5e64fba3240d554e6f884
chuangxin <chuangxin5567@163.com>
2020-04-15 16:37:55 +0800

    日志

--- src/main/java/com/cx/controller/UserController.java
+++ src/main/java/com/cx/controller/UserController.java
@@ -21,6 +21,8 @@ public class UserController {
         return userService.findAll();
     }
 
+
+    //测试路径http://localhost:8081/day01/user/save?name=李祺&age=55&bir=2020/04/15
     @GetMapping("save")
     public void save(User user){
         userService.save(user);
